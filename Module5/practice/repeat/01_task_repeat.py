f6868f236fb1b5a05cc0e6b1a02a6f46699d7bc5
YolkaAkh <108610675+YolkaAkh@users.noreply.github.com>
2022-07-11 20:50:48 +0300

    Update 01_task_repeat.py
    наконец-то я это сделала))) почему-то долго не могла понять как объединить и размер списка, и диапазон

--- Module5/practice/repeat/01_task_repeat.py
+++ Module5/practice/repeat/01_task_repeat.py
@@ -2,6 +2,15 @@
 # произвольными целыми числами в заданном диапазоне.
 # , где size - размер генерируемого списка c элементами в диапазоне от of до to.
 
-def gen_list(size, of, to):
-    pass
+import random
+lst = []
+def gen_list(of, to, size):
+    for i in range(size):
+        lst.append(random.randint(of, to))
+    return lst
 
+a = 0
+b = 15
+c = 5
+result = gen_list(a,b,c)
+print(result)
